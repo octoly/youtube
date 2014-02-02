554daee2c1daa85dffcba136a27a5ce3f22b4223
Fabien G <fabien@sent.com>
2014-02-02 16:06:46 +0100

    removing useless & non-understandable error :)

--- lib/youtube/error.rb
+++ lib/youtube/error.rb
@@ -7,11 +7,6 @@ module Youtube
       @errors ||= Hash[descendants.map{|klass| [klass.const_get(:HTTP_STATUS_CODE), klass]}]
     end
 
-    # @return [Array]
-    def self.descendants
-      ObjectSpace.each_object(::Class).select{|klass| klass < self}
-    end
-
     # Initializes a new Error object
     #
     # @param exception [Exception, String]
